beb2a2a2ed47e82ce936b36b73c54e4a2956f6a8
MaquinoBSC <martinare.ipn@gmail.com>
2021-06-23 00:29:34 -0500

    Instalacion de React Navigation 5 y creando el sistemas de navegacion

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import {SafeAreaView, Text} from 'react-native';
 import {Provider as PaperProvider, Button} from 'react-native-paper';
+import {NavigationContainer} from '@react-navigation/native';
+import Navigation from './src/navigation/Navigation';
 
 export default function App(){
 
@@ -9,17 +11,9 @@ export default function App(){
 
   return(
     <PaperProvider>
-      <SafeAreaView>
-        <Text>Hola</Text>
-        <Button 
-          loading={loading} 
-          icon="fingerprint" 
-          mode="contained" 
-          onPress={()=> console.log("Presionado")}
-        >
-          Press Me
-        </Button>
-      </SafeAreaView>
+      <NavigationContainer>
+        <Navigation />
+      </NavigationContainer>
     </PaperProvider>
   )
 }
