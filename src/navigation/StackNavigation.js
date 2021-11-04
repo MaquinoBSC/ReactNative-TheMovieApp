86dc92789c263a281368d321fb3f9197196bd465
MaquinoBSC <martinare.ipn@gmail.com>
2021-11-04 17:00:38 -0600

    Añadir el boton de busqueda en el header

--- src/navigation/StackNavigation.js
+++ src/navigation/StackNavigation.js
@@ -13,40 +13,65 @@ const Stack= createStackNavigator();
 export default function StackNavigation(props){
     const {navigation}= props;
 
-    const buttonLeft= ()=> {
-        return(
+    const buttonLeft= (screen)=> {
+        switch(screen){
+            case "search":
+            case "movie":
+                return (
+                    <IconButton
+                        icon="arrow-left"
+                        onPress={()=> navigation.goBack()} 
+                    />
+                )
+
+            default: 
+                return (
+                    <IconButton
+                        icon="menu"
+                        onPress={()=> navigation.openDrawer()} 
+                    />
+                )
+            
+            
+        }
+    }
+
+    const buttonRight= ()=> {
+        return (
             <IconButton
-                icon="menu"
-                onPress={()=> navigation.openDrawer()} 
+                icon="magnify"
+                onPress={()=> navigation.navigate('search')}
             />
         )
     }
+
+
     return(
         <Stack.Navigator>
             <Stack.Screen
                 name="home"
                 component={Home}
-                options={{title: "The Movie App", headerLeft: ()=> buttonLeft()}}
+                options={{title: "The Movie App", headerLeft: ()=> buttonLeft('home'), headerRight: ()=> buttonRight()}}
             />
             <Stack.Screen
                 name="movie"
                 component={Movie} 
-                options={{title: '', headerLeft: ()=> buttonLeft()}}
+                options={{title: '', headerLeft: ()=> buttonLeft('movie'), headerRight: ()=> buttonRight()}}
             />
             <Stack.Screen
                 name="news"
                 component={News} 
-                options={{title: "Nuevas Peliculas", headerLeft: ()=> buttonLeft()}}
+                options={{title: "Nuevas Peliculas", headerLeft: ()=> buttonLeft('news'), headerRight: ()=> buttonRight()}}
             />
             <Stack.Screen
                 name="popular"
                 component={Popular} 
-                options={{title: "Peliculas Populares", headerLeft: ()=> buttonLeft()}}
+                options={{title: "Peliculas Populares", headerLeft: ()=> buttonLeft('popular'), headerRight: ()=> buttonRight()}}
             />
             <Stack.Screen
                 name="search"
                 component={Search}
-                options={{title: '', headerLeft: ()=> buttonLeft()}}
+                options={{title: '', headerLeft: ()=> buttonLeft('search')}}
             />
         </Stack.Navigator>
     )
