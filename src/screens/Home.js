dacd7fa5db83673997f969e5de6259e79e6ee43f
MaquinoBSC <martinare.ipn@gmail.com>
2021-11-05 11:53:57 -0600

    Obteniendo las ultimas peliculas publicadas

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {View, Text} from 'react-native';
+import { getNewsMoviesApi } from '../api/movies';
 
 export default function Home(){
+    const [newMovies, setNewMovies]= useState(null);
+    console.log(newMovies);
+
+    useEffect(async() => {
+      const data= await getNewsMoviesApi();
+      setNewMovies(data.results);
+    }, []);
+
     return(
         <View>
             <Text>Estamos en Home</Text>
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,3 @@
+export const API_HOST= 'https://api.themoviedb.org/3';
 export const API_KEY= "24895dfb48389b52753abdb267dbed81";
+export const LANG= 'es-ES';
