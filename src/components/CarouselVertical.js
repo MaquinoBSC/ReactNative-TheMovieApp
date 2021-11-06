294910e2400ea9dfb5ba54d0253b714cabfd25f9
MaquinoBSC <martinare.ipn@gmail.com>
2021-11-05 19:04:56 -0600

    Agregar carousel que muestra las peliculas nuevas

---
+++ src/components/CarouselVertical.js
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, View, Image, Dimensions, TouchableWithoutFeedback, Text } from 'react-native';
+import Carousel from 'react-native-snap-carousel';
+
+
+const { width }= Dimensions.get('window');
+const ITEM_WIDTH= Math.round(width * 0.7);
+
+export default function CarouselVertical(props){
+    const { data }= props;
+
+    return (
+        <Carousel 
+            layout={"default"}
+            data={data}
+            renderItem={(movie)=> <RenderItem data={movie} />}
+            sliderWidth={width}
+            itemWidth={ITEM_WIDTH}
+        />
+    )
+}
+
+function RenderItem(props){
+    const {}= props;
+
+    return (
+        <View>
+            <Text>Pelicula</Text>
+        </View>
+    )
+}
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,10 +1,12 @@
 import React, {useState, useEffect} from 'react';
-import {View, Text} from 'react-native';
+import { View, Text, StyleSheet, ScrollView } from 'react-native';
+import { Title } from 'react-native-paper';
+import CarouselVertical from '../components/CarouselVertical';
 import { getNewsMoviesApi } from '../api/movies';
+
 
 export default function Home(){
     const [newMovies, setNewMovies]= useState(null);
-    console.log(newMovies);
 
     useEffect(async() => {
       const data= await getNewsMoviesApi();
@@ -12,8 +14,27 @@ export default function Home(){
     }, []);
 
     return(
-        <View>
-            <Text>Estamos en Home</Text>
-        </View>
+        <ScrollView showsVerticalScrollIndicator={false}>
+            {
+                newMovies && (
+                    <View style={styles.news}>
+                        <Title style={styles.newsTitle}>Nuevas Películas</Title>
+                        <CarouselVertical data={newMovies} />
+                    </View>
+                )
+            }
+        </ScrollView>
     )
 }
+
+const styles= StyleSheet.create({
+    news: {
+        marginVertical: 10,
+    },
+    newsTitle: {
+        marginBottom: 15,
+        marginHorizontal: 20,
+        fontWeight: 'bold',
+        fontSize: 22,
+    }
+});
