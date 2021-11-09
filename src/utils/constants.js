9a39ef519f829194e8d482e22feb7e6dcc3bb4e2
MaquinoBSC <martinare.ipn@gmail.com>
2021-11-08 21:30:38 -0600

    Pintando las peliculas en el carousel

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,4 @@
 export const API_HOST= 'https://api.themoviedb.org/3';
 export const API_KEY= "24895dfb48389b52753abdb267dbed81";
 export const LANG= 'es-ES';
+export const BASE_PATH_IMG= 'https://image.tmdb.org/t/p';
