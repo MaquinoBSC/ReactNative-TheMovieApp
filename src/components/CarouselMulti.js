b10ba5068858b951af11025429d1cffab333a2f2
MaquinoBSC <martinare.ipn@gmail.com>
2021-11-16 21:02:52 -0600

    Renderizand las pelculas en el carousel

--- src/components/CarouselMulti.js
+++ src/components/CarouselMulti.js
@@ -18,18 +18,56 @@ export default function CarouselMulti(props){
             data={data}
             renderItem={(item)=> <RenderItem data={item} />}
             sliderWidth={width}
-            itemWidth={ITEM_WIDTH} 
+            itemWidth={ITEM_WIDTH}
+            firstItem={1}
+            inactiveSlideScale={1}
+            inactiveSlideOpacity={1}
         />
     )
 }
 
 
-function RenderItem(){
+function RenderItem(props){
+    const { data }= props;
+    const { id, title, poster_path }= data.item;
     const navigation= useNavigation();
+    const imageUrl= `${BASE_PATH_IMG}/w500${poster_path}`;
+
+    const onNavigation= ()=>{
+        navigation.navigate('movie', { id })
+    }
 
     return (
-        <View>
-            <Title>Hola</Title>
-        </View>
+        <TouchableWithoutFeedback onPress={()=> onNavigation()}>
+            <View style={styles.card}>
+                <Image style={styles.image} source={{uri: imageUrl}} />
+                <Title style={styles.title} numberOfLines={1}> { title } </Title>
+            </View>
+        </TouchableWithoutFeedback>
     )
 }
+
+
+const styles= StyleSheet.create({
+    card: {
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 10
+        },
+        shadowOpacity: 1,
+        shadowRadius: 10
+    },
+
+    image:{
+        width: '85%',
+        height: 170,
+        borderRadius: 20,
+    },
+
+    title: {
+        marginHorizontal: 10,
+        marginTop: 10,
+        fontSize: 16,
+    }
+})
