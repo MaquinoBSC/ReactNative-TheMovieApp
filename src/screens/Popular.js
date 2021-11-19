b270d1102c51818f91e4ff99a01b3f05badb6dfe
MaquinoBSC <martinare.ipn@gmail.com>
2021-11-19 17:06:49 -0600

    Obtener las peliculas populares

--- src/screens/Popular.js
+++ src/screens/Popular.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {View, Text} from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { getPopularMovieApi } from '../api/movies';
 
 export default function Popular(){
+    const navigation= useNavigation();
+    const [movies, setMovies]= useState(null);
+    console.log(movies);
+
+    useEffect(()=> {
+        getPopularMovieApi(1).then((response)=> {
+            setMovies(response.results);
+        })
+    }, []);
+
     return(
         <View>
             <Text>Estamso en Popular</Text>
