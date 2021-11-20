a9123a2fd3aefcf5b26eb995893f7d4ccea0b9b4
MaquinoBSC <martinare.ipn@gmail.com>
2021-11-19 20:59:58 -0600

    Renderizar el poster de las peliculas populares

--- src/screens/Popular.js
+++ src/screens/Popular.js
@@ -1,12 +1,15 @@
 import React, { useState, useEffect } from 'react';
-import {View, Text} from 'react-native';
+import {View, StyleSheet, ScrollView, Image } from 'react-native';
+import { Text } from 'react-native-paper';
+import { map } from 'lodash';
 import { useNavigation } from '@react-navigation/native';
 import { getPopularMovieApi } from '../api/movies';
+import { BASE_PATH_IMG } from '../utils/constants';
+import noImage from '../assets/jpg/default-imgage.png';
 
 export default function Popular(){
     const navigation= useNavigation();
     const [movies, setMovies]= useState(null);
-    console.log(movies);
 
     useEffect(()=> {
         getPopularMovieApi(1).then((response)=> {
@@ -15,8 +18,49 @@ export default function Popular(){
     }, []);
 
     return(
-        <View>
-            <Text>Estamso en Popular</Text>
+        <ScrollView>
+            {
+                map(movies, (movie, index)=> (
+                    <Movie key={index} movie={movie} />
+                ))
+            }
+        </ScrollView>
+    )
+}
+
+
+function Movie(props){
+    const { movie }= props;
+    const { poster_path }= movie;
+    return (
+        <View style={styles.movie}>
+            <View style={styles.left}>
+                <Image 
+                    style={styles.image} 
+                    source={
+                        poster_path ? { uri: `${BASE_PATH_IMG}/w500${poster_path}` } : noImage
+                    } 
+                />
+            </View>
+            <View>
+                <Text>RIGHT</Text>
+            </View>
         </View>
     )
 }
+
+
+const styles= StyleSheet.create({
+    movie: {
+        marginBottom: 20,
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    left: {
+        marginRight: 20,
+    },
+    image: {
+        width: 100,
+        height: 150,
+    }
+});
