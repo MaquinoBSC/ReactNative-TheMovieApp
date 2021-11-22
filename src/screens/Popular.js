794b72836ce1cee63699e1b4c88e1e375288a0f5
MaquinoBSC <martinare.ipn@gmail.com>
2021-11-22 15:49:19 -0600

    Añadiendo la informacion y puntacion de cada pelicula en la seccion de Peliculas populares

--- src/screens/Popular.js
+++ src/screens/Popular.js
@@ -1,15 +1,21 @@
 import React, { useState, useEffect } from 'react';
 import {View, StyleSheet, ScrollView, Image } from 'react-native';
-import { Text } from 'react-native-paper';
+import { Text, Title } from 'react-native-paper';
+import { Rating } from 'react-native-ratings';
 import { map } from 'lodash';
 import { useNavigation } from '@react-navigation/native';
 import { getPopularMovieApi } from '../api/movies';
 import { BASE_PATH_IMG } from '../utils/constants';
+import usePreferences from '../hooks/usePreferences';
 import noImage from '../assets/jpg/default-imgage.png';
+import starDark from '../assets/png/starDark.png';
+import starLight from '../assets/png/starLight.png';
+
 
 export default function Popular(){
     const navigation= useNavigation();
     const [movies, setMovies]= useState(null);
+    const { theme }= usePreferences();
 
     useEffect(()=> {
         getPopularMovieApi(1).then((response)=> {
@@ -21,7 +27,7 @@ export default function Popular(){
         <ScrollView>
             {
                 map(movies, (movie, index)=> (
-                    <Movie key={index} movie={movie} />
+                    <Movie key={index} movie={movie} theme={theme} />
                 ))
             }
         </ScrollView>
@@ -30,8 +36,9 @@ export default function Popular(){
 
 
 function Movie(props){
-    const { movie }= props;
-    const { poster_path }= movie;
+    const { movie, theme }= props;
+    const { poster_path, title, release_date, vote_count, vote_average }= movie;
+
     return (
         <View style={styles.movie}>
             <View style={styles.left}>
@@ -43,8 +50,31 @@ function Movie(props){
                 />
             </View>
             <View>
-                <Text>RIGHT</Text>
+                <Title> { title } </Title>
+                <Text> { release_date } </Text>
+                <MovieRating voteCount={vote_count} voteAverage={vote_average} theme={theme} />
             </View>
+        </View>
+    )
+}
+
+
+function MovieRating(props){
+    const { voteCount, voteAverage, theme }= props;
+    const media= voteAverage / 2;
+
+    return (
+        <View style={styles.viewRating}>
+            <Rating
+                type="custom"
+                ratingImage={ theme=== 'dark' ? starDark : starLight }
+                ratingColor="#ffc205"
+                ratingBackgroundColor={ theme== 'dark' ? "#192734" : '#f0f0f0'} 
+                startingValue={ media }
+                imageSize={20}
+                style={{ marginRight: 15 }}
+            />
+            <Text style={{ fontSize: 12, color:"#8697a5", marginTop: 5 }}> { voteCount } votos</Text>
         </View>
     )
 }
@@ -62,5 +92,10 @@ const styles= StyleSheet.create({
     image: {
         width: 100,
         height: 150,
+    },
+    viewRating: {
+        alignItems: 'flex-start',
+        justifyContent: 'flex-start',
+        marginTop: 10,
     }
 });
