e4e05a6ebc61123a5f123be76edd989766ba2a64
MaquinoBSC <martinare.ipn@gmail.com>
2021-11-23 16:28:44 -0600

    Boton para cargar mas peliculas y navegar a la screen movie para visualizar la info de la pelicula

--- src/screens/News.js
+++ src/screens/News.js
@@ -4,22 +4,37 @@ import { Button, Text } from 'react-native-paper';
 import { useNavigation } from '@react-navigation/native';
 import { map } from 'lodash';
 import { getNewsMoviesApi } from '../api/movies';
+import usePreferences from '../hooks/usePreferences';
 import { BASE_PATH_IMG } from '../utils/constants';
 
 
 const {width}= Dimensions.get('window');
 
 
-export default function News(props){
-    const navigation= useNavigation();
+export default function News(){
     const [movies, setMovies]= useState(null);
     const [page, setPage]= useState(1);
+    const [showBtnMore, setShowBtnMore]= useState(true);
+    const { theme }= usePreferences();
+
 
     useEffect(()=> {
         getNewsMoviesApi(page).then((response)=> {
-            setMovies(response.results);
+            const totalPages= response.total_pages;
+            
+            if(page < totalPages){
+                if(!movies){
+                    setMovies(response.results);
+                }
+                else{
+                    setMovies([...movies, ...response.results]);
+                }
+            }
+            else{
+                setShowBtnMore(false);
+            }
         });
-    }, []);
+    }, [page]);
 
 
     return(
@@ -31,6 +46,17 @@ export default function News(props){
                     ))
                 }
             </View>
+            {
+                showBtnMore && (
+                    <Button 
+                        mode="contained"
+                        contentStyle={styles.loadMoreContainer}
+                        style={styles.loadMore}
+                        labelStyle={{ color: theme== 'dark' ? "#fff" : '#000'}}
+                        onPress={()=> setPage(page + 1)}
+                    > Cargar mas ... </Button>
+                )
+            }
         </ScrollView>
     )
 }
@@ -38,21 +64,28 @@ export default function News(props){
 function Movie(props){
     const { movie }= props;
     const { id, poster_path, title }= movie;
+    const navigation= useNavigation();
+
+    const onNavigation= ()=> {
+        navigation.navigate('movie', { id });
+    }
 
     return (
-        <View style={styles.movie}>
-            {
-                poster_path ? (
-                    <Image 
-                        style={styles.image}
-                        source={{ uri: `${BASE_PATH_IMG}/w500${poster_path}` }}
-                    />
-                ) : (
-                    <Text>{title}</Text>
-                )
-            }
-            <Text>{ title }</Text>
-        </View>
+        <TouchableWithoutFeedback onPress={()=> onNavigation()}>
+            <View style={styles.movie}>
+                {
+                    poster_path !== null ? (
+                        <Image 
+                            style={styles.image}
+                            source={{ uri: `${BASE_PATH_IMG}/w500${poster_path}` }}
+                        />
+                    ) : (
+                        <Text style={{ width: 200, height: 200 }}>{title}</Text>
+                    )
+                }
+                {/* <Text>{ title }</Text> */}
+            </View>
+        </TouchableWithoutFeedback>
     )
 }
 
@@ -71,5 +104,12 @@ const styles= StyleSheet.create({
     image: {
         width: '100%',
         height: '100%'
+    },
+    loadMoreContainer: {
+        paddingTop: 10,
+        paddingBottom: 30,
+    },
+    loadMore: {
+        backgroundColor: 'transparent',
     }
 })
