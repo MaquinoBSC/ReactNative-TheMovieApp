5cb1cb10b1dff13ab4edd1e410a57a7963efb488
MaquinoBSC <martinare.ipn@gmail.com>
2021-11-23 15:53:20 -0600

    Renderizando las peliculas en la seccion de nuevas peliculas

--- src/screens/News.js
+++ src/screens/News.js
@@ -4,6 +4,10 @@ import { Button, Text } from 'react-native-paper';
 import { useNavigation } from '@react-navigation/native';
 import { map } from 'lodash';
 import { getNewsMoviesApi } from '../api/movies';
+import { BASE_PATH_IMG } from '../utils/constants';
+
+
+const {width}= Dimensions.get('window');
 
 
 export default function News(props){
@@ -20,22 +24,52 @@ export default function News(props){
 
     return(
         <ScrollView>
-            {
-                map(movies, (movie, idx)=> (
-                    <Movie key={idx} movie={movie} />
-                ))
-            }
+            <View style={styles.container}>
+                {
+                    map(movies, (movie, idx)=> (
+                        <Movie key={idx} movie={movie} />
+                    ))
+                }
+            </View>
         </ScrollView>
     )
 }
 
 function Movie(props){
     const { movie }= props;
-    const { title }= movie;
+    const { id, poster_path, title }= movie;
 
     return (
-        <View>
+        <View style={styles.movie}>
+            {
+                poster_path ? (
+                    <Image 
+                        style={styles.image}
+                        source={{ uri: `${BASE_PATH_IMG}/w500${poster_path}` }}
+                    />
+                ) : (
+                    <Text>{title}</Text>
+                )
+            }
             <Text>{ title }</Text>
         </View>
     )
 }
+
+const styles= StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+    },
+    movie: {
+        width: width / 2,
+        height: 300,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    image: {
+        width: '100%',
+        height: '100%'
+    }
+})
