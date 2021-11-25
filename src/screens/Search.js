e175f0cf90e621794d90852e2f1ef4d87c7442a6
MaquinoBSC <martinare.ipn@gmail.com>
2021-11-24 22:01:26 -0600

    Agregar un input para crear el buscado

--- src/screens/Search.js
+++ src/screens/Search.js
@@ -1,20 +1,40 @@
 import React, { useState, useEffect } from 'react';
-import {View, Text} from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Image, Platform, TouchableWithoutFeedback, Dimensions} from 'react-native';
+import { Searchbar } from 'react-native-paper';
+import { size } from 'lodash';
 import { searchMoviesApi } from '../api/movies';
+
 
 export default function Search(){
     const [movies, setMovies]= useState(null);
+    const [search, setSearch]= useState("");
+    console.log(movies);
 
     useEffect(()=> {
-        searchMoviesApi("batman").then((response)=> {
-            setMovies(response.results);
-        });
-    }, []);
+        if(size(search) > 2 ){
+            searchMoviesApi(search).then((response)=> {
+                setMovies(response.results);
+            });
+        }
+    }, [ search ]);
 
 
     return(
-        <View>
-            <Text>Estamos en Search</Text>
-        </View>
+        <SafeAreaView>
+            <Searchbar
+                placeholder="Busca tu pelicula" 
+                iconColor={ Platform.OS == 'ios' && 'transparent'}
+                icon="arrow-left"
+                style={styles.input}
+                onChangeText={(e)=> setSearch(e)}
+            />
+        </SafeAreaView>
     )
 }
+
+const styles= StyleSheet.create({
+    input: {
+        marginTop: -3,
+        backgroundColor: '#15212b',
+    }
+});
